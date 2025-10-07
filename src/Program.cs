0e9b19b391785a931c8bfe1e0fe4e28fbd8d37d2
Ryan Hallisey <hallisey.ryan@gmail.com>
2025-10-07 18:04:36 -0400

    Get All Lobbies

--- src/Program.cs
+++ src/Program.cs
@@ -7,9 +7,12 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 var app = builder.Build();
 
+app.MapGet("/lobbies", async (LobbyServiceDb db) =>
+    await db.Lobbies.ToListAsync());
+
 app.MapGet("/lobbies/{id}", async (Guid id, LobbyServiceDb db) =>
-await db.Lobbies.FindAsync(id) is Lobby lobby
-    ? Results.Ok(lobby)
-    : Results.NotFound());
+    await db.Lobbies.FindAsync(id) is Lobby lobby
+        ? Results.Ok(lobby)
+        : Results.NotFound());
 
 app.Run();
