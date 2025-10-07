91735bbad222e085c76832442e95eae2002c5566
Ryan Hallisey <hallisey.ryan@gmail.com>
2025-10-07 18:01:57 -0400

    Get Lobby

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<LobbyServiceDb>(options => options.UseInMemoryDatabase("LobbyDb"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/lobbies/{id}", async (Guid id, LobbyServiceDb db) =>
+await db.Lobbies.FindAsync(id) is Lobby lobby
+    ? Results.Ok(lobby)
+    : Results.NotFound());
 
 app.Run();
