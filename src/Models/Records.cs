1eef40d8738adda7f35335d7cfb25f8e13e0bd41
Ryan Hallisey <hallisey.ryan@gmail.com>
2025-10-08 01:57:31 -0400

    Define Lobby Member Primary Key

--- src/Models/Records.cs
+++ src/Models/Records.cs
@@ -1,7 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+
 public record User(Guid Id, string UserName, string PasswordHash, DateTimeOffset CreatedAt);
 
 public record Lobby(Guid Id, Guid Owner, string Name, string Status);
 
+[PrimaryKey(nameof(LobbyId), nameof(UserId))]
 public record LobbyMember(Guid LobbyId, Guid UserId, DateTimeOffset JointedAt);
 
 public record ChatMessage(Guid Id, Guid LobbyId, Guid UserId, string Message, DateTimeOffset Timestamp);
